9656f102b75798b233ad662515ff974c00669a63
WhiteDog12 <WhiteDog12@6c7b080e-ad16-0410-bd3c-fb41285370c0>
2006-08-17 15:38:00 +0000

    Modified to use an indexer
    git-svn-id: https://nhapi.svn.sourceforge.net/svnroot/nhapi@18 6c7b080e-ad16-0410-bd3c-fb41285370c0

--- nHapi/nHapi.Base/Model/GenericComposite.cs
+++ nHapi/nHapi.Base/Model/GenericComposite.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+
 namespace NHapi.Base.model
 {
 	
@@ -11,20 +13,40 @@ namespace NHapi.Base.model
 	/// </author>
 	public class GenericComposite:AbstractType, IComposite
 	{
+        private ArrayList components;
+        private IMessage message;
+
 		/// <summary> Returns an array containing the components of this field.</summary>
 		virtual public IType[] Components
 		{
 			get
 			{
-				IType[] ret = new IType[components.Count];
+                IType[] ret = new IType[components.Count];
 				for (int i = 0; i < ret.Length; i++)
 				{
 					ret[i] = (IType) components[i];
 				}
 				return ret;
+
 			}
 			
 		}
+
+        ///// <summary> Returns the single component of this composite at the specified position (starting at 0) - 
+        ///// Creates it (and any nonexistent components before it) if necessary.  
+        ///// </summary>
+        public IType this[int index]
+        {
+            get
+            {
+                for (int i = components.Count; i <= index; i++)
+                {
+                    components.Add(new Varies(message));
+                }
+                return (IType)components[index];
+            }
+
+        }
 		/// <summary>Returns the name of the type (used in XML encoding and profile checking)  </summary>
 		override public System.String Name
 		{
@@ -35,26 +57,25 @@ namespace NHapi.Base.model
 			
 		}
 		
-		private System.Collections.ArrayList components;
-		private IMessage message;
+		
 		
 		/// <summary>Creates a new instance of GenericComposite </summary>
 		public GenericComposite(IMessage message):base(message)
 		{
 			this.message = message;
-			components = new System.Collections.ArrayList(20);
+            components = new ArrayList();
 		}
 		
-		/// <summary> Returns the single component of this composite at the specified position (starting at 0) - 
-		/// Creates it (and any nonexistent components before it) if necessary.  
-		/// </summary>
-		public virtual IType getComponent(int number)
-		{
-			for (int i = components.Count; i <= number; i++)
-			{
-				components.Add(new Varies(message));
-			}
-			return (IType) components[number];
-		}
+        ///// <summary> Returns the single component of this composite at the specified position (starting at 0) - 
+        ///// Creates it (and any nonexistent components before it) if necessary.  
+        ///// </summary>
+        //public virtual IType getComponent(int number)
+        //{
+        //    for (int i = components.Count; i <= number; i++)
+        //    {
+        //        components.Add(new Varies(message));
+        //    }
+        //    return (IType) components[number];
+        //}
 	}
 }
