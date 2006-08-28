0c66e8cd1baae705db8218b18fad7ad21c50147a
whitedog12 <whitedog12@6c7b080e-ad16-0410-bd3c-fb41285370c0>
2006-08-28 17:41:35 +0000

    git-svn-id: https://nhapi.svn.sourceforge.net/svnroot/nhapi@51 6c7b080e-ad16-0410-bd3c-fb41285370c0

--- NHapi20/NHapi.Base/Properties/Settings.Designer.cs
+++ NHapi20/NHapi.Base/Properties/Settings.Designer.cs
@@ -9,10 +9,21 @@
 //------------------------------------------------------------------------------
 using System.Configuration;
 using System;
-namespace NHapi.Base.Properties
-{
-    internal sealed partial class Settings
-    {
+
+namespace NHapi.Base.Properties {
+    
+    
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "8.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
+        }
 
         public static string ConnectionString
         {
