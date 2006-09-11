f813adbb1ff4a967097709a778595a491c539c51
whitedog12 <whitedog12@6c7b080e-ad16-0410-bd3c-fb41285370c0>
2006-09-11 18:12:40 +0000

    Added parameter for connection string
    git-svn-id: https://nhapi.svn.sourceforge.net/svnroot/nhapi@86 6c7b080e-ad16-0410-bd3c-fb41285370c0

--- NHapi20/ModelGenerator/Program.cs
+++ NHapi20/ModelGenerator/Program.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using System.Text;
 using NHapi.Base.SourceGeneration;
 
+
 namespace ModelGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //args = new string[2];
-            //args[0] = @"C:\Projects\nHapiRestructured\nHapi\Test";
-            //args[1] = "2.3.1";
-            if (2 != args.Length)
+            //args = new string[3];
+            //args[0] = @"C:\Projects\Nhapi\Nhapi20";
+            //args[1] = "2.5";
+            //args[2] = @"C:\Projects\hl7\Database\hl7db.mdb";
+
+            
+            if (args.Length<2)
             {
-                System.Console.Error.WriteLine("Not enough arguments.");
+
+                System.Console.Error.WriteLine("Not enough arguments ({0}).", args.Length.ToString());
                 PrintUsage();
                 Environment.Exit(1);
+
             }
 
+            if (args.Length == 3)
+            {
+                string conn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + args[2];
+                NHapi.Base.NormativeDatabase.Instance.OpenNewConnection(conn);
+            }
+
+            Console.WriteLine("Using Database{0}", NHapi.Base.NormativeDatabase.Instance.Connection.ConnectionString);
             SourceGenerator.makeAll(args[0], args[1]);
 
         }
