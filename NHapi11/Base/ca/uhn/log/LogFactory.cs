74762a8ec67aab013a426256be09fe5097b5ba38
whitedog12 <whitedog12@6c7b080e-ad16-0410-bd3c-fb41285370c0>
2006-10-18 03:34:24 +0000

    git-svn-id: https://nhapi.svn.sourceforge.net/svnroot/nhapi@117 6c7b080e-ad16-0410-bd3c-fb41285370c0

---
+++ NHapi11/Base/ca/uhn/log/LogFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Text;
+
+namespace ca.uhn.log
+{
+	/// <summary>
+	/// Placeholder for LogFactory to get project to compile.
+	/// </summary>
+	class LogFactory
+	{
+		public static Log getLog(System.Type classType)
+		{
+			return new EntLibLogger();
+		}
+
+		public static Log getLog(System.String name)
+		{
+			return new EntLibLogger();
+		}
+	}
+}
