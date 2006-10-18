71274dcc5752444e4e4a95acd635411d2e6a521d
whitedog12 <whitedog12@6c7b080e-ad16-0410-bd3c-fb41285370c0>
2006-10-18 03:35:13 +0000

    git-svn-id: https://nhapi.svn.sourceforge.net/svnroot/nhapi@119 6c7b080e-ad16-0410-bd3c-fb41285370c0

---
+++ NHapi11/Base/ca/uhn/hl7v2/model/GenericPrimitive.cs
@@ -0,0 +1,59 @@
+using System;
+namespace ca.uhn.hl7v2.model
+{
+	
+	/// <summary> An unspecified Primitive datatype that imposes no constraints on its string 
+	/// value.  This is used to store Varies data, when the data type is unknown.  It is also 
+	/// used to store unrecognized message constituents.  
+	/// </summary>
+	/// <author>  Bryan Tripp
+	/// </author>
+	public class GenericPrimitive:AbstractPrimitive, Primitive
+	{
+		//UPGRADE_NOTE: Respective javadoc comments were merged.  It should be changed in order to comply with .NET documentation conventions. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1199'"
+		/// <summary> Returns a String representation of the value of this field.</summary>
+		/// <summary> Sets the value of this field if the given value is legal in the context of the
+		/// implementing class.
+		/// </summary>
+		/// <throws>  DataTypeException if the given value is not valid in this context. </throws>
+		override public System.String Value
+		{
+			get
+			{
+				return this.value_Renamed;
+			}
+			
+			set
+			{
+				this.value_Renamed = value;
+			}
+			
+		}
+		/// <summary>Returns the name of the type (used in XML encoding and profile checking)  </summary>
+		override public System.String TypeName
+		{
+			get
+			{
+				return "UNKNOWN";
+			}
+			
+		}
+		/// <seealso cref="ca.uhn.hl7v2.model.Primitive.getVersion()">
+		/// </seealso>
+		virtual public System.String Version
+		{
+			get
+			{
+				return null;
+			}
+			
+		}
+		
+		internal System.String value_Renamed = null;
+		
+		/// <summary> Creates a new instance of GenericPrimitive </summary>
+		public GenericPrimitive(Message message):base(message)
+		{
+		}
+	}
+}
