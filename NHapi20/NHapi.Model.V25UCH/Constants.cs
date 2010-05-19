ab44bcdc03293433b24ea3afca2956a856a16c61
whitedog12 <whitedog12@6c7b080e-ad16-0410-bd3c-fb41285370c0>
2010-05-19 14:30:49 +0000

    Fixed UCH classes
    git-svn-id: https://nhapi.svn.sourceforge.net/svnroot/nhapi@162 6c7b080e-ad16-0410-bd3c-fb41285370c0

---
+++ NHapi20/NHapi.Model.V25UCH/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NHapi.Model.V25UCH
+{
+    public class Constants
+    {
+        public const string VERSION_25_UCH = "2.5.UCH";
+    }
+}
